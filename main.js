1a995cb7fa83169422f4d9c1a56a4f47522fb225
Jose David <josedavidgm1995@gmail.com>
2018-10-11 16:38:56 -0500

    Color picker

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let onMobile =  (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-
+let cube;
 let body = document.querySelector("body"); console.log("Hi!");
 
 function app() {
@@ -8,7 +8,6 @@ function app() {
     body.innerHTML = `
 
                 <div class="container" >
-                    
                     <div id="app-advised">    
                         <img id="phone" src="images/smartphone.png">     
                         <br>
@@ -16,38 +15,31 @@ function app() {
                         <br>
                         <br> 2) Agrégala a tu pantalla de <br>inicio como una App.</h2>                  
                     </div>
-
                     <div id="turn-phone">
-                        <img id="phone" src="images/smartphone.png">     
+                        <img id="turn-smartphone" src="images/turn-smartphone.png">     
                         <br>
                         <h2> Alto ahí loca, gira tu celular :v</h2>                  
                     </div> 
-    
-                </div> 
-
-                
-
-                   
-                <!--h2 >Abre la app en tu celular y agrégala a tu inicio</h2-->
-                         
+                </div>                          
                      
-
-                     <div id="title">
-                         
-                         <img src="images/logo.png">
-                     </div>
-                     <div id="info">
-                         <p>By Jose David GM - 10/10/2018</p>
-                         <p>Follow me on 
-                         <a href="https://www.behance.net/JoseDavidGiraldoM" target=" _blank">Behance</a> 
-                         / <a href="https://www.instagram.com/_david.giraldo/" target=" _blank">Instagram<a></p>
-                     </div>
-                       
+                <div id="title">
+                    <img src="images/logo.png">
+                </div>
+                <div id="info">
+                      <p>By Jose David GM - 10/10/2018</p>
+                      <p>Follow me on 
+                      <a href="https://www.behance.net/JoseDavidGiraldoM" target=" _blank">Behance</a> 
+                    / <a href="https://www.instagram.com/_david.giraldo/" target=" _blank">Instagram<a></p>
+                </div>
+                <div id="cubeContainer">
+                <div id="cube"></div>   
+                </div>    
                         `;
                         console.log(onMobile);
     if(onMobile == true){
         body.querySelector('#app-advised').style.display = "none";
     }
+    cube = body.querySelector('#cube');
 
 }
 
--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 let canvas;
-let r, g, b, amp, inc, valG, valR, valB;
+let r, g, b, amp, inc, valG, valR, valB, radius, h, s, valH, valS;
+let hipotenusa, catetoOpuesto, x, y;
 
 function setup() {
 
     frameRate(60);
     canvas = createCanvas(windowWidth, windowHeight);
-    canvas.style('z-index', '-1');
+    canvas.style('z-index', '4');
     canvas.style('position', 'fixed');
     canvas.style('top', '0');
     valR = 0;
@@ -13,40 +14,65 @@ function setup() {
     valB = 0;
     r = 0;
     g = 0;
-    b = 0;
+    h = 359;
+    s = 100;
+    b = 50;
     amp = 155;
     inc = 0.01;
-    deviceOrientation = 'portrait';
+
+    cube.style.width = `${windowWidth * 0.9}`;
+    cube.style.height = `${windowWidth * 0.9}`;
+    cube.style.display = "inline";
+    //359,100,100
+    //colorMode(HSB, 100);
+
 }
 
 function draw() {
-    fullscreen();
+    background(0, 0);
+    clear();
 
-    background(0);
-
-
-    valR = 100+(sin(r += inc)) * amp;
+    /*valR = 100+(sin(r += inc)) * amp;
     valB = 100+(sin(b += inc)) * amp;
     valG = 100+(sin(g += 0.08)) * amp;
+    valR = dist(displayWidth / 2, displayHeight / 2, mouseX, mouseY);
+    r = map(valR, 0, windowWidth * 0.9, 0, 255);
 
-    fill(255, 0, valB);
+    valB = dist(displayWidth / 2, displayHeight / 2, mouseX, mouseY);
+    b = map(valR, 0, windowWidth * 0.9, 255,0);*/
 
-    noStroke();
     if (windowWidth > windowHeight) {
 
-        if(onMobile == true){
+        if (onMobile == true) {
             body.querySelector('#turn-phone').style.display = "flex";
+            cube.style.display = "none";
         }
 
     } else {
-        if(onMobile == true){
+        if (onMobile == true) {
             body.querySelector('#turn-phone').style.display = "none";
         }
-        ellipse(windowWidth / 2, windowHeight / 2, windowWidth * 0.9, windowWidth * 0.9);
-    }
+        cube.style.display = "inline";
 
-    newCursor();
+    }
+    hipotenusa = dist(displayWidth / 2, displayHeight / 2, mouseX, mouseY);
+    h = degrees(sin(y / hipotenusa));
+    cube.style.background = `hsl(${h},${100}%,${50}%)`;
     
+    x = (mouseX - windowWidth / 2)*1;
+    y = (mouseY - windowHeight / 2)*-1;
+    /*text("H: " + hipotenusa, mouseX + 50, mouseY + 30);
+    text("X: " + x, mouseX + 50, mouseY + 50);
+    text("Y: " + y, mouseX + 50, mouseY + 70);
+    stroke(255);
+    line(displayWidth / 2, displayHeight / 2, mouseX, mouseY);
+    text(sin(y / hipotenusa), mouseX + 50, mouseY + 90);*/
+    
+    
+
+
+    
+    newCursor();
 
 }
 
@@ -56,25 +82,11 @@ function isMobileDevice() {
 
 function windowResized() {
     resizeCanvas(displayWidth, displayHeight);
- 
-}
 
-function deviceTurned() {
-    resizeCanvas(displayWidth, displayHeight);
-  
 }
 
 function newCursor() {
     noStroke();
     fill(255);
     ellipse(pmouseX, pmouseY, 10, 10);
-}
-
-
-function mousePressed() {
-
-}
-
-function mouseReleased() {
-
 }
